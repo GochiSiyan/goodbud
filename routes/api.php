009c3d9936f3703a5276117fc27591637399ce54
gochi <gochi.siyan@gmail.com>
2023-03-05 23:48:09 +0800

    rearrange user route

--- routes/api.php
+++ routes/api.php
@@ -19,15 +19,12 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout']);
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    });
-});
-
-Route::middleware(['auth'])->group(function () {
     Route::apiResource('user', UserController::class);
     Route::apiResource('products', ProductController::class);
     Route::apiResource('order', OrderController::class);
     Route::apiResource('category', ProductCategoryController::class);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
 });
