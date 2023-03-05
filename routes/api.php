503859b07129f838973f7987c5ec2e5f97b7cb18
gochi <gochi.siyan@gmail.com>
2023-03-06 00:20:40 +0800

    add route class import

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,10 @@
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProductCategoryController;
 
 /*
 |--------------------------------------------------------------------------
